3b1ee469a4ff52fd185adb2685c55f632b97f571
raiska <raisa-translation@yandex.ru>
2020-12-26 10:26:49 +0300

    Created empty project

---
+++ pushkin/pushkin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pushkin
+//
+//  Created by RAISA on 26.12.2020.
+//  Copyright © 2020 RAISA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
